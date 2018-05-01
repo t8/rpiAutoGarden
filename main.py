83a29af50eb94f5f8270ebc1aec3c8b57a54212c
tbaumer22 <tateflyer1@gmail.com>
2018-05-01 16:21:52 -0400

    WOO HOOO got a basic timer working!

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import os
-import time
 from threading import Timer
 
-now = time.localtime()
-hour = now.tm_hour
-
-
 def initialization():
-    # if hour == 16:
-    #     startWatering(0,1)
     # SHOULD BE 28800 SECS WHICH EQUALS 8 HOURS
-    waitingToWater = Timer(10.0, startWatering(0, 1))
+    waitingToWater = Timer(10.0, startWatering, [0, 1])
     waitingToWater.start()
 
 
 def startWatering(stack, relayNum):
     os.system("megaio " + str(stack) + " rwrite " + str(relayNum) + " on")
+    os.system("echo 'NOW WATERING THE PLANTS'")
     # SHOULD BE 12 SECS
-    needToStop = Timer(12.0, stopWatering(stack,relayNum))
+    needToStop = Timer(12.0, stopWatering, [stack,relayNum])
     needToStop.start()
 
 
 def stopWatering(stack, relayNum):
     os.system("megaio " + str(stack) + " rwrite " + str(relayNum) + " off")
-    # os.system("echo 'STOPPING WATERING PROCESS'")
+    os.system("echo 'JUST FINISHED WATERING THE PLANTS'")
+    initialization()
 
 
 initialization()
