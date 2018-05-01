e6975a8a11752229cd591b8025ffdab57541dcd6
tate <tateflyer1@gmail.com>
2018-05-01 16:05:58 -0400

    more progress on python script

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import os
+import time
 from threading import Timer
+
+now = time.localtime()
+hour = now.tm_hour
 
 
 def initialization():
+    # if hour == 16:
+    #     startWatering(0,1)
     # SHOULD BE 28800 SECS WHICH EQUALS 8 HOURS
     waitingToWater = Timer(10.0, startWatering(0, 1))
     waitingToWater.start()
 
+
 def startWatering(stack, relayNum):
-    os.system(("megaio " + stack + " rwrite " + relayNum + " on"))
+    os.system("megaio " + str(stack) + " rwrite " + str(relayNum) + " on")
     # SHOULD BE 12 SECS
-    needToStop = Timer(12.0, stopWatering(stack, relayNum))
+    needToStop = Timer(12.0, stopWatering(stack,relayNum))
     needToStop.start()
 
+
 def stopWatering(stack, relayNum):
-    os.system(("megaio " + stack + " rwrite " + relayNum + " off"))
+    os.system("megaio " + str(stack) + " rwrite " + str(relayNum) + " off")
+    # os.system("echo 'STOPPING WATERING PROCESS'")
+
+
+initialization()
