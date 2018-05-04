b2e882f290ddd2f82d7db89269e836ff2c69d4ce
tate <tateflyer1@gmail.com>
2018-05-03 20:59:02 -0400

    optimized code and finished implementing weather conditionals

--- main.py
+++ main.py
@@ -9,31 +9,29 @@ lookup = weather.lookup(2487365)
 condition = lookup.condition
 
 needToWaterMore = False
+triggerConditions = ["tropical storm", "showers", "hail", "sleet", "mixed rain and hail", "hot", "scattered showers", "scattered showers", "thundershowers"]
 
 
 def initialization():
-    schedule.every().day.at("11:30").do(startWatering, [0, 1])
-    updateWeather()
+    schedule.every().day.at("1:30").do(startWatering, [0, 1])
     schedule.every().hour.do(updateWeather)
 
 
 def updateWeather():
     lookup = weather.lookup(2487365)
     condition = lookup.condition
-    print(condition.text)
-    if condition != "Showers" & condition != "Thundershowers" & condition != "scattered showers":
-        needToWaterMore = True
-    else:
+    lowerCaseCondition = condition.text.lower()
+
+    if triggerConditions.count(lowerCaseCondition) > 0:
         needToWaterMore = False
-    if condition == "cloudy":
-        print("THIS WORKS REGARDLESS OF CASE")
+    else:
+        needToWaterMore = True
 
 
 def startWatering(stack, relayNum):
     os.system("megaio " + str(stack) + " rwrite " + str(relayNum) + " on")
     print("NOW WATERING THE PLANTS")
-    # SHOULD BE 12 SECS
-    needToStop = Timer(12.0, stopWatering, [stack,relayNum])
+    needToStop = Timer(15.0, stopWatering, [stack,relayNum])
     needToStop.start()
     if needToWaterMore == True:
         waterMore = Timer(20.0, startWatering, [0, 1])
