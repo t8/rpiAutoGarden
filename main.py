d74b11d7a091c1743c8ac85b73e67d96d68aff0d
tate <tateflyer1@gmail.com>
2018-05-04 22:15:48 -0400

    syntax improvements/ deleted unneeded function

--- main.py
+++ main.py
@@ -8,16 +8,14 @@ weather = Weather(unit=Unit.CELSIUS)
 lookup = weather.lookup(2487365)
 condition = lookup.condition
 
-needToWaterMore = False
+needToWaterMore = True
 triggerConditions = ["tropical storm", "showers", "hail", "sleet", "mixed rain and hail", "scattered showers", "scattered showers", "thundershowers"]
 
 
-def initialization():
-    schedule.every().day.at("1:30").do(startWatering, [0, 1])
-    schedule.every().hour.do(updateWeather)
-
-
 def updateWeather():
+    global needToWaterMore
+    global lookup
+    global condition
     lookup = weather.lookup(2487365)
     condition = lookup.condition
     lowerCaseCondition = condition.text.lower()
@@ -33,7 +31,7 @@ def startWatering(stack, relayNum):
     print("NOW WATERING THE PLANTS")
     needToStop = Timer(15.0, stopWatering, [stack, relayNum])
     needToStop.start()
-    if needToWaterMore == True:
+    if needToWaterMore:
         schedule.every().day.at('15:30').do(startWatering, [stack, relayNum]).tag('extra-cycle')
     else:
         schedule.clear('extra-cycle')
@@ -42,10 +40,10 @@ def startWatering(stack, relayNum):
 def stopWatering(stack, relayNum):
     os.system("megaio " + str(stack) + " rwrite " + str(relayNum) + " off")
     print("FINISHED WATERING THE PLANTS")
-    initialization()
 
 
-initialization()
+schedule.every().day.at("1:30").do(startWatering, [0, 1])
+schedule.every().hour.do(updateWeather)
 while True:
     schedule.run_pending()
     time.sleep(1)
